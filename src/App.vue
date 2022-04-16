d7ff1c6c598721ad158140c21858ee7e8fb699f2
sperjar <65524316+sperjar@users.noreply.github.com>
2022-04-16 20:44:10 +0800

    testview1

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/test1">Test1</router-link>
   </nav>
   <router-view/>
 </template>
